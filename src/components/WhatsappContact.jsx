d87a17c18e83bcae343297427b99e3b203e3f9fc
auntfunny <greenauntfunny@gmail.com>
2026-04-13 18:04:33 -0400

    Whatsapp size change

--- src/components/WhatsappContact.jsx
+++ src/components/WhatsappContact.jsx
@@ -1,8 +1,8 @@
 function WhatsappContact(){
     return(
-        <div className="sticky top-[95%] z-30 w-11 place-self-end">
+        <div className="sticky top-[95%] z-30 w-11 md:w-14 lg:w-18 place-self-end">
             <a href="https://wa.me/56996284956" target="_blank"> {/* random number added*/}
-                <img src="/images/whatsapp.png" alt="whatsapp logo" className="size-11 absolute right-1 bottom-1"/>
+                <img src="/images/whatsapp.png" alt="whatsapp logo" className="size-11 md:size-14 lg:size-18 absolute right-1 bottom-1"/>
             </a>
         </div>
     )
